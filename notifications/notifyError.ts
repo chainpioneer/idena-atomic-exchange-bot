36ef228e9942fd61d40202e7bc5e5677a1b5f20e
chainpioneer <chainpioneer@gmail.com>
2023-04-03 10:17:10 -0300

    Upd

--- notifications/notifyError.ts
+++ notifications/notifyError.ts
@@ -1,16 +1,16 @@
-import sendMessageToPublic from "./sendMessageToPublic";
+import sendMessageToSentry from './sendMessageToSentry'
 
 export default async function notifyError(err: string, exit = false) {
     if (!exit) {
         if (err.length < 400) {
-            await sendMessageToPublic(err)
+            await sendMessageToSentry(err)
         }
-        await sendMessageToPublic(`Long error produced, see console`)
+        await sendMessageToSentry(`Long error produced, see console`)
     } else {
         if (err.length < 400) {
-            await sendMessageToPublic(err)
+            await sendMessageToSentry(err)
         }
-        await sendMessageToPublic(`Long error produced, see console`)
+        await sendMessageToSentry(`Long error produced, see console`)
         process.exit(1)
     }
 }
